52b74075b289706de890f4700a0758c4059d6ad6
Rick Leinecker <rick@leinecker.com>
2024-07-01 02:19:41 -0400

    trying here

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -54,7 +54,7 @@ export default function Login()
       <span id="inner-title">PLEASE LOG IN</span><br />
       <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} />
       <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} />
-      <input type="submit" id="loginButton" class="buttons" value = "Do It"
+      <input type="submit" id="loginButton" className="buttons" value = "Do It"
         onClick={doLogin} />
       <span id="loginResult">{message}</span>
     </div>
